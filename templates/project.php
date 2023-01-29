b36a01ef2b8b69737fcaa4866f72a8451e243adc
Fanky4ert <Fanky4ert@gmail.com>
2023-01-29 18:23:47 +0200

    lesson3

---
+++ templates/project.php
@@ -0,0 +1,8 @@
+<a href="index.php" class="nav-link active">
+    <i class="nav-icon fas fa-columns"></i>
+    <p>
+        <?=htmlentities($description);?>
+        Назва проекту
+        <span class="badge badge-info right">2</span>
+    </p>
+</a>
