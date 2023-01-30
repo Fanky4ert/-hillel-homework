7959d6a3cc896c273f2d3e618ba107dcc770145c
Fanky4ert <Fanky4ert@gmail.com>
2023-01-30 15:24:36 +0200

    optimize

--- index.php
+++ index.php
@@ -4,9 +4,19 @@ include_once ('templates/helpers.php');
 $project = renderTemplate('project.php');
 $down = renderTemplate('footer.php');
 $card = renderTemplate('card1.php');
-$vstavka = renderTemplate('main.php',['card1'=>$card]);
 
-$website = renderTemplate('layout.php',['content'=>$vstavka,'sitename'=>'мой сайт','soname'=>'Savchenko','project'=>$project,'footer'=>$down]);
+$vstavka = renderTemplate('main.php',
+    [
+        'card1'=>$card,
+    ]);
+
+$website = renderTemplate('layout.php',
+    [
+        'content'=>$vstavka,
+        'site_name'=>'мой сайт',
+        'soname'=>'Savchenko',
+        'project'=>$project,
+    ]);
 
 print $website;
 
--- templates/layout.php
+++ templates/layout.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
-        <?=$sitename?>
+        <?=$site_name?>
     </title>
 
     <!-- Google Font: Source Sans Pro -->
