0bd116bce04e9e6956602be15ec65cf7ccad7087
Fanky4ert <Fanky4ert@gmail.com>
2023-01-30 14:27:04 +0200

    finish result

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 include_once ('templates/helpers.php');
-$arr = [
-    'headsite'=>'школа php',
-    'savchenko'=>'Савченко',
-];
-extract($arr);
 
 $project = renderTemplate('project.php');
 $down = renderTemplate('footer.php');
 $card = renderTemplate('card1.php');
 $vstavka = renderTemplate('main.php',['card1'=>$card]);
 
-$website = renderTemplate('layout.php',['content'=>$vstavka,'project'=>$project,'sitename'=>$headsite,'soname'=>$savchenko,'footer'=>$down]);
+$website = renderTemplate('layout.php',['content'=>$vstavka,'sitename'=>'мой сайт','soname'=>'Savchenko','project'=>$project,'footer'=>$down]);
 
 print $website;
 
--- templates/layout.php
+++ templates/layout.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$sitename?>
+    <title>
+        <?=$sitename?>
     </title>
 
     <!-- Google Font: Source Sans Pro -->
@@ -94,7 +95,14 @@
 
     <?=$content?>
 
-    <?=$footer?>
+    <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 0.1.0
+    </div>
+    <strong>Copyright &copy; 2023 <a href="https://ithillel.ua/">Комп'ютерна школа Hillel</a>.</strong> All rights
+    reserved.
+    </footer>
+
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
