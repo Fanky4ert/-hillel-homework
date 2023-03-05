a68b671fc832dd1260576422974d69137ec20b04
Fanky4ert <Fanky4ert@gmail.com>
2023-03-05 23:04:51 +0200

    task8

--- templates/task-add.php
+++ templates/task-add.php
@@ -105,39 +105,3 @@
             </form>
         </section>
     </div>
-
-    <footer class="main-footer">
-        <div class="float-right d-none d-sm-block">
-            <b>Version</b> 0.1.0
-        </div>
-        <strong>Copyright &copy; 2023 <a href="https://ithillel.ua/">Комп'ютерна школа Hillel</a>.</strong> All rights
-        reserved.
-    </footer>
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="static/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="static/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Bootstrap -->
-<script src="static/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Ekko Lightbox -->
-<script src="static/plugins/ekko-lightbox/ekko-lightbox.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="static/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="static/js/adminlte.min.js"></script>
-<!-- Filterizr-->
-<script src="static/plugins/filterizr/jquery.filterizr.min.js"></script>
-<!-- Page specific script -->
-<script src="static/js/kanban.js"></script>
-</body>
-
-</html>
